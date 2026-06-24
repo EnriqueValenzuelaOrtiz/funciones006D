3d77748f1da7c104adeea7601184190f36622cf0
EnriqueValenzuelaOrtiz <enri.valenzuela@duocuc.cl>
2026-06-24 16:58:15 -0400

    si

--- Prueba2_main.py
+++ Prueba2_main.py
@@ -1,4 +1,4 @@
-import 06_24_EV_Repaso_Prueba as p 
+import Prueba2 as p
 #Código Principal
 
 #Declarar Lista
@@ -7,4 +7,55 @@ lista_reservas=[]
 opcion=0
 while opcion!=6:
     #Mostrar Menú
-    import p.mostrar_menu
+    p.mostrar_menu()
+    #Preguntamos Opción
+    opcion = p.ingresar_opcion()
+
+    if opcion==1:
+        #Llamamos Función Agregar Reservas
+        p.agregar_reserva(lista_reservas)
+    
+    elif opcion==2:
+        #Solicitar Nombre
+        nombre=input("Ingrese el nombre del huésped a buscar: \n")
+        pos=p.buscar_reserva(lista_reservas, nombre)
+        #Validamos
+        if pos !=-1:
+            #Mostramos los Datos
+            print(f"Huésped: {lista_reservas[pos]["huesped"]}")
+            print(f"Habitación: {lista_reservas[pos]["habitacion"]}")
+            print(f"Noches: {lista_reservas[pos]["noches"]}")
+            estado="Confirmado" if lista_reservas[pos]["confirmada"] else "Pendiente"
+            print(f"Estado: {estado}")
+
+        else:
+            print(f"El husped {nombre} no ha sido encontrado")
+
+    elif opcion==3:
+         #Solicitar Nombre
+        nombre=input("Ingrese el nombre del huésped a eliminar: \n")
+        pos=p.buscar_reserva(lista_reservas, nombre)
+        #Validamos
+        if pos !=-1:
+            #Elimino el elemnto de la Lista
+            lista_reservas.pop(pos)
+            print("Reserva eliminada correctamente")
+        
+        else:
+            print(f"El husped {nombre} no ha sido encontrado")
+
+    elif opcion==4:
+        #Llamar Función Actualizar Confirmación
+        p.confirmar_reservas(lista_reservas)
+        print("Reservas actualizadas correctamente")
+
+    elif opcion==5:
+        p.confirmar_reservas(lista_reservas)
+        p.mostrar_reservas(lista_reservas)
+
+    elif opcion==6:
+        print("Gracias por utilizar el sistema")
+        
+
+
+        
