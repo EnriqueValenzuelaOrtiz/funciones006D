9a4e8e6b40606ebb14cdbf428d135910a37604a7
EnriqueValenzuelaOrtiz <enri.valenzuela@duocuc.cl>
2026-06-24 16:22:49 -0400

    si

--- Prueba2_main.py
+++ Prueba2_main.py
@@ -1,2 +1,10 @@
+import 06_24_EV_Repaso_Prueba as p 
 #Código Principal
-import  as p 
+
+#Declarar Lista
+lista_reservas=[]
+
+opcion=0
+while opcion!=6:
+    #Mostrar Menú
+    import p.mostrar_menu
